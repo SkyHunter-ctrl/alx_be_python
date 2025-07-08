3ef4e96d06e7c063bcf870e9fa46d3aaab74e04f
Patrick Saiyua <ledemas09@gmail.com>
2025-07-08 22:36:56 +0100

    Updated rectangle file

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,7 +1,7 @@
 # Definition of terms
-"lenght = 10"
-"width = 5"
+length = 10
+width = 5
 # Calculation of area
-"area = lenght * width"
+area = length * width
 print("The area of the rectangle is: {area}")
 
