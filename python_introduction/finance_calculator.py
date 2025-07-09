40142b38e08fe7124747c18cf60c3d2dd6a32eae
Patrick Saiyua <ledemas09@gmail.com>
2025-07-09 15:52:52 +0100

    Projected annual saving

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,6 @@
+# Personal Finance Calculator
+income = int(input(“Enter your monthly income: ”))
+expenses = int(input(“Enter your total monthly expenses: ”))
+savings = income - expenses
+Projected Savings = savings * 12 + (savings * 12 * 0.05)
+
