e84cd81110256db074aa9898903e21d82eaa715b
SkyHunter <ledemas09@gmail.com>
2025-08-08 15:15:57 +0100

    Library

--- oop/library_system.py
+++ oop/library_system.py
@@ -12,6 +12,7 @@ class Book:
 # Derived classes EBook and PrintBook
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size: int):
+        super().__init__(title, author)
         self.title = title
         self.author = author
         self.file_size = file_size
@@ -21,7 +22,7 @@ class EBook(Book):
         return self.get_details()
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
-        super
+        super().__init__(title, author)
         self.title = title
         self.author = author
         self.page_count = page_count
