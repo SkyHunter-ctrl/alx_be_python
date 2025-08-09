9244e3ec68b98415b17386070efb3322df3f0fc4
SkyHunter <ledemas09@gmail.com>
2025-08-09 14:51:06 +0100

    polymorphism

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,32 @@
+# understanding of polymorphism in Python by creating a set of classes that demonstrate method overriding and polymorphic behavior
+# Base class
+import math
+class Shape:
+    def area(self):
+        raise NotImplementedError
+
+# Derived classes
+class Rectangle(Shape):
+    def __init__(self, length: float, width: float):
+        self.lenght = length
+        self.width = width
+    def area(self):
+        return self.lenght * self.width
+class Circle(Shape):
+    def __init__(self, radius: float):
+        self.radius = radius
+    def area(self) -> float:
+        return math.pi * self.radius ** 2
+def main():
+    shapes = [
+        Rectangle(10, 5),
+        Circle(7)
+    ]
+    
+    for shape in shapes:
+        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+    if __name__ == "__main__":
+        main()
+
+
+        
